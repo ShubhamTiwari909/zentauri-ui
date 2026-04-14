cc47777ffdfe8b69316dacb33902b7df9823ff7b
ShubhamTiwari909 <shubhmtiwri00@gmail.com>
2026-04-14 13:14:40 +0530

    code cleanups and nitpicks

--- apps/component-library/components/home/use-home.ts
+++ apps/component-library/components/home/use-home.ts
@@ -1,12 +1,7 @@
 import { homeContent } from "./data";
-import { HomeData } from "./types";
 
 export function useHome() {
-  const getHomeContent = (): HomeData => {
-    return homeContent;
-  };
-
   return {
-    content: getHomeContent(),
+    content: homeContent,
   };
 }
